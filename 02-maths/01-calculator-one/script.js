5cafecde2dbfb644a8e4609766968950baf9e6c0
Ortega <ortega.victor@gmail.com>
2020-07-13 10:32:58 +0200

    Change <span> to <input> for show  results/ calc32

--- 02-maths/01-calculator-one/script.js
+++ 02-maths/01-calculator-one/script.js
@@ -9,67 +9,62 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    let result=0;
+    let result = 0;
 
-    document.getElementById("addition").addEventListener("click", function() {
+    document.getElementById("addition").addEventListener("click", function () {
         // Validating operands
-           if(isNaN(document.getElementById("op-one").value) || isNaN(document.getElementById("op-two").value))
-           {
-                document.getElementById("result").textContent="Enter Valid operands"
-           }else{
-                // perform an addition
-                result=parseFloat(document.getElementById("op-one").value) + parseFloat(document.getElementById("op-two").value);
-                document.getElementById("result").textContent=result;
-                }
+        if (isNaN(document.getElementById("op-one").value) || isNaN(document.getElementById("op-two").value)) {
+            document.getElementById("result").value = "Enter Valid operands"
+        } else {
+            // perform an addition
+            result = parseFloat(document.getElementById("op-one").value) + parseFloat(document.getElementById("op-two").value);
+            document.getElementById("result").value = result;
+        }
     });
 
-    document.getElementById("substraction").addEventListener("click", function() {
+    document.getElementById("substraction").addEventListener("click", function () {
 
-         // Validating operands
-            if(isNaN(document.getElementById("op-one").value)||isNaN(document.getElementById("op-two").value))
-            {
-                document.getElementById("result").textContent="Enter Valid operands"
+        // Validating operands
+        if (isNaN(document.getElementById("op-one").value) || isNaN(document.getElementById("op-two").value)) {
+            document.getElementById("result").value = "Enter Valid operands"
 
-            }else{
-                // perform an substraction
-                result=parseFloat(document.getElementById("op-one").value) - parseFloat(document.getElementById("op-two").value);
-                document.getElementById("result").textContent=result;
-                }
+        } else {
+            // perform an substraction
+            result = parseFloat(document.getElementById("op-one").value) - parseFloat(document.getElementById("op-two").value);
+            document.getElementById("result").value = result;
+        }
     });
 
-    document.getElementById("multiplication").addEventListener("click", function() {
+    document.getElementById("multiplication").addEventListener("click", function () {
 
         // Validating operands
 
-           if(isNaN(document.getElementById("op-one").value)||isNaN(document.getElementById("op-two").value))
-           {
-            document.getElementById("result").textContent="Enter Valid operands"
-        } else{
+        if (isNaN(document.getElementById("op-one").value) || isNaN(document.getElementById("op-two").value)) {
+            document.getElementById("result").value = "Enter Valid operands"
+        } else {
 
             // perform an multiplication
-            result=parseFloat(document.getElementById("op-one").value) * parseFloat(document.getElementById("op-two").value);
-            document.getElementById("result").textContent=result;
-             }
+            result = parseFloat(document.getElementById("op-one").value) * parseFloat(document.getElementById("op-two").value);
+            document.getElementById("result").value = result;
+        }
     });
 
-    document.getElementById("division").addEventListener("click", function() {
+    document.getElementById("division").addEventListener("click", function () {
 
-         // Validating operands
+        // Validating operands
 
-        if(isNaN(document.getElementById("op-one").value)||isNaN(document.getElementById("op-two").value))
-          {
-            document.getElementById("result").textContent="Enter Valid operands"
-          }else{
+        if (isNaN(document.getElementById("op-one").value) || isNaN(document.getElementById("op-two").value)) {
+            document.getElementById("result").value = "Enter Valid operands"
+        } else {
             // perform an division
-            if (parseFloat(document.getElementById("op-two").value) == 0)
-            {
-                document.getElementById("result").textContent="Division by 0";
-            }else{
-                    result=parseFloat(document.getElementById("op-one").value) / parseFloat(document.getElementById("op-two").value);
-                    document.getElementById("result").textContent=result;
-                }
-              }
+            if (parseFloat(document.getElementById("op-two").value) == 0) {
+                document.getElementById("result").value = "Division by 0";
+            } else {
+                result = parseFloat(document.getElementById("op-one").value) / parseFloat(document.getElementById("op-two").value);
+                document.getElementById("result").value = result;
+            }
+        }
     });
 })();
